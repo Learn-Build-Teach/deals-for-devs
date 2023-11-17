10570c49448eafa3061b72a4c1029ec921dae6b4
jamesqquick <james.q.quick@gmail.com>
2023-11-17 09:58:44 -0600

    Make email required

--- src/app/deals/add/DealForm.tsx
+++ src/app/deals/add/DealForm.tsx
@@ -10,7 +10,7 @@ const schema = z.object({
   endDate: z.coerce.date(),
   coupon: z.string().optional(),
   couponPercentage: z.number().optional(),
-  email: z.string().email().optional().or(z.literal('')),
+  email: z.string().email(),
 });
 
 export default function DealForm() {
@@ -171,14 +171,14 @@ export default function DealForm() {
             htmlFor="email"
             aria-label="email"
           >
-            Your email (in case there are any follow up details needed)?
-            (optional)
+            Your email? (in case there are any follow up details needed)
           </label>
           <input
             className="shadow appearance-none border-2 border-gray-700 rounded w-full py-2 px-3 text-gray-300 leading-tight focus:outline-none focus:shadow-outline bg-transparent "
             name="email"
             id="email"
             type="text"
+            required
           />
         </div>
         <button className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-32">
