5c96bae05a1586f7e1ba2f533f550cd420edf8fc
jamesqquick <james.q.quick@gmail.com>
2023-11-20 13:52:35 -0600

    Centering content with grid

--- src/app/DealsList.tsx
+++ src/app/DealsList.tsx
@@ -11,7 +11,7 @@ export default async function DealsList({
   return (
     <div>
       {deals.length > 0 && (
-        <div className="grid grid-cols-1 justify-items-center w-full sm:grid-cols-2 md:grid-cols-3  gap-4 ">
+        <div className="grid grid-cols-1 sm:justify-items-stretch justify-items-center w-full sm:grid-cols-2 md:grid-cols-3  gap-4 ">
           {deals.map((deal) => (
             <Deal key={deal.id} deal={deal} showAdminOptions={isAdmin} />
           ))}
