4a04acbb21d0c62f25c3a036fc05e66d2ee90a22
jamesqquick <james.q.quick@gmail.com>
2023-12-28 18:21:07 -0500

    Checked for posthog env variables

---
+++ .environment.d.ts
@@ -0,0 +1,14 @@
+export namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: String;
+    CLERK_SECRET_KEY: String;
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: String;
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: String;
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: String;
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: String;
+    XATA_BRANCH: String;
+    XATA_API_KEY: String;
+    NEXT_PUBLIC_POSTHOG_KEY: String;
+    NEXT_PUBLIC_POSTHOG_HOST: String;
+  }
+}
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,7 +5,11 @@ import { PostHogProvider } from 'posthog-js/react';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 
-if (typeof window !== 'undefined') {
+if (
+  typeof window !== 'undefined' &&
+  process.env.NEXT_PUBLIC_POSTHOG_KEY &&
+  process.env.NEXT_PUBLIC_POSTHOG_HOST
+) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     capture_pageview: false, // Disable automatic pageview capture, as we capture manually
