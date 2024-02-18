c3fd621bab882ca9238172937466bf236da0d0d5
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-02-18 16:54:34 -0500

    Refactor: middleware public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,21 +4,21 @@ import { authMiddleware } from '@clerk/nextjs'
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-	publicRoutes: [
-		'/',
-		'/deals/(.*)',
-		'/deals',
-		'/thank-you',
-		'/monitoring',
-		'/api/deals',
-		'/.well-known/webfinger',
-		'/test/e',
-		'/confirm-email',
-		'/subscriber-preferences',
-		'/api/cron/monthly-newsletter',
-	],
+  publicRoutes: [
+    '/',
+    '/deals/(.*)',
+    '/deals',
+    '/thank-you',
+    '/monitoring',
+    '/api/deals',
+    '/.well-known/webfinger',
+    '/confirm',
+    '/preferences',
+    '/api/cron/monthly-newsletter',
+    '/validate',
+  ],
 })
 
 export const config = {
-	matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 }
