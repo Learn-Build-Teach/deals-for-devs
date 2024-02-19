15d43a42cec8569e4ff4c97b165fd23e9d5df11b
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-02-19 00:49:28 -0500

    Update padding of buttons in subscriber preferences page

--- src/components/subscriber/Subscriber.tsx
+++ src/components/subscriber/Subscriber.tsx
@@ -84,7 +84,7 @@ export default function Subscriber({
           </button>
 
           <button
-            className="rounded-md border border-red-400 bg-transparent px-3.5 py-2.5 text-center text-xs font-light text-red-400 shadow-sm hover:bg-red-400 hover:text-red-900 md:py-4 md:text-[28px]"
+            className="rounded-md border border-red-400 bg-transparent px-3.5 py-2.5 text-center text-xs font-light text-red-400 shadow-sm hover:bg-red-400 hover:text-red-900 md:py-6 md:text-[28px]"
             onClick={() => updateSubscriptions(false)}
           >
             Unsubscribe from all
@@ -105,7 +105,7 @@ export default function Subscriber({
         </ul>
 
         <button
-          className="mb-80 mt-9 w-full rounded-md bg-teal-600 py-3 text-center text-sm font-semibold text-black shadow-sm hover:bg-teal-400 disabled:cursor-not-allowed disabled:border  disabled:border-teal-500 disabled:bg-transparent disabled:text-teal-500 md:mt-20 md:py-4 md:text-2xl"
+          className="mb-80 mt-9 w-full rounded-md bg-teal-600 py-3 text-center text-sm font-semibold text-black shadow-sm hover:bg-teal-400 disabled:cursor-not-allowed disabled:border  disabled:border-teal-500 disabled:bg-transparent disabled:text-teal-500 md:mt-20 md:py-5 md:text-2xl"
           onClick={async () => {
             await updatePreferences(subscriberData.id, subscriberData)
             toast.success('Preferences updated!')
