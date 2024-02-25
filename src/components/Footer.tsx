a26701abb274f7f039c2e8ed2d00592c47638554
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-02-25 16:27:37 -0500

    Refactor tailwind utility classes in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,42 +2,44 @@ import Link from 'next/link'
 import Icon from './Icon'
 import Image from 'next/image'
 
-type Sponsor = {
+type TechStack = {
   Url: string
   id: string
   size: number
   className?: string
 }
 
-const techStack: Sponsor[] = [
+const techStack: TechStack[] = [
   {
     Url: 'https://nextjs.org/',
     id: 'nextjs',
     size: 173.58,
-    className: 'w-[88.54px] h-[14.83px] md:w-[173.58px] md:h-[29.08px]',
+    className: 'w-20 h-3.5 md:w-44 md:h-7',
   },
   {
     Url: 'https://xata.io/',
     id: 'xata',
     size: 142,
-    className: 'w-[88px] h-[37px] md:w-[142px] md:h-[40px]',
+    className: 'w-20 h-9 md:w-36 md:h-10',
   },
   {
     Url: 'https://xata.io/',
     id: 'sentry',
     size: 221,
-    className: 'w-[113px] h-[34px] md:w-[221px] md:h-[66px]',
+    className: 'w-28 h-8 md:w-56 md:h-16',
   },
 ]
 
 export default function Footer() {
   return (
-    <div className="bg-[#0C111C] px-[80px] py-9 md:px-0 md:py-[36px]">
-      <div className="mx-auto flex max-w-4xl flex-col items-center">
+    <div className="bg-[#0C111C] py-9">
+      <div className="mx-auto flex max-w-56 flex-col items-center md:max-w-4xl">
         <span className="mb-8 text-sm font-medium leading-8 text-white md:text-3xl">
           Built With
         </span>
-        <div className="flex flex-wrap items-center gap-4 md:gap-10">
+
+        {/* built with logos */}
+        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-10">
           {techStack.map((sponsor, index) => (
             <a key={index} href={sponsor.Url} target="_blank">
               <Icon
@@ -53,7 +55,7 @@ export default function Footer() {
             src="/icons/clerk.svg"
             width={159}
             height={49}
-            className="h-[25px] w-[81px] md:h-[49px] md:w-[159px]"
+            className="h-6 w-20 md:h-12 md:w-40"
             alt="clerk.dev logo"
           />
         </div>
