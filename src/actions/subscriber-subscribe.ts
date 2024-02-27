2d140bbf1c8992a0b031bd8b1baeaee732b7f8f4
jamesqquick <james.q.quick@gmail.com>
2024-02-26 19:53:43 -0600

    Error handling on initial subscribe

--- src/actions/subscriber-subscribe.ts
+++ src/actions/subscriber-subscribe.ts
@@ -2,26 +2,19 @@
 import { v4 as uuidv4 } from 'uuid'
 import { z } from 'zod'
 import { sendConfirmationEmail } from '../utils/resend/email-sendConfirmation'
-import { redirect } from 'next/navigation'
 import { createValidateEmailLink, createConfirmEmailLink } from '@/lib/utils'
-import {
-  createSubscriber,
-  getOneSubscriberByEmail,
-  getOneSubscriberByToken,
-} from '@/lib/queries'
+import { createSubscriber, getOneSubscriberByEmail } from '@/lib/queries'
 
 const subscribeSchema = z.object({
   email: z.string().email(),
 })
 
 export const subscribe = async (formData: FormData) => {
-  let parsed
-
-  // create unique token
-  const token = uuidv4()
   try {
+    const token = uuidv4()
+
     // parse the email
-    parsed = subscribeSchema.parse({ email: formData.get('email') })
+    const parsed = subscribeSchema.parse({ email: formData.get('email') })
 
     const existingSubscriber = await getOneSubscriberByEmail(parsed.email)
     if (existingSubscriber) {
@@ -47,7 +40,13 @@ export const subscribe = async (formData: FormData) => {
 
     // send confirmation email using resend
     const validateEmailLink = createValidateEmailLink(token)
-    await sendConfirmationEmail(parsed.email, validateEmailLink)
+    const { error } = await sendConfirmationEmail(
+      parsed.email,
+      validateEmailLink
+    )
+    if (error) {
+      return { error: 'Failed to send confirmation email.' }
+    }
     return { data: token }
   } catch (error) {
     //TODO: handle specific errors
