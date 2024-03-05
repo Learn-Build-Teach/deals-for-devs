364681939c6acce066c5bfc71b5849d3c6dba689
Ellie <ellieslife@icloud.com>
2024-03-05 17:05:37 -0400

    Change to tailwind classes & add custom classes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,14 +4,14 @@ export default function Hero() {
   return (
     <div className="px-6 md:px-0">
       <div className="flex flex-col gap-6 md:gap-12 md:pb-32 lg:gap-16 xl:flex-row">
-        <h1 className="max-w-80 text-4xl font-semibold leading-10 tracking-tight text-white md:max-w-[650px] md:text-6xl md:leading-[67px] lg:text-7xl xl:max-w-[861px]">
+        <h1 className="md:leading-16 xl:max-w-3xl-plus max-w-80 text-4xl font-semibold leading-10 tracking-tight text-white md:max-w-2xl md:text-6xl lg:text-7xl">
           The best <span className="text-teal-500">deals</span> and{' '}
           <span className="text-teal-500">giveaways</span> for developers
         </h1>
 
         {/* subscription form */}
         <div>
-          <p className="mb-3 text-sm leading-5 text-white md:mb-6 md:max-w-[550px] md:text-xl md:leading-8 lg:text-2xl">
+          <p className="mb-3 text-sm leading-5 text-white md:mb-6 md:max-w-lg md:text-xl md:leading-8 lg:max-w-xl lg:text-2xl">
             Get upcoming and ongoing deals sent straight to your inbox every
             month!
           </p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,13 @@ const config = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      lineHeight: {
+        '16': '4.2rem',
+      },
+      maxWidth: {
+        '3xl-plus': '860px',
+        'screen-3xl': '1728px',
+      }
     },
   },
   plugins: [require('tailwindcss-animate')],
