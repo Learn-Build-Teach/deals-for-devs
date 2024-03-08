4ce174beeca08adc7d7939da70cb89e4e5d5a3f3
jamesqquick <james.q.quick@gmail.com>
2024-03-08 13:51:23 -0600

    test: adding sentry inp

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -22,7 +22,7 @@ Sentry.init({
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
     new Sentry.Replay({
-      // Additional Replay configuration goes in here, for example:
+      // Additional Replay configuration goes in here:
       maskAllText: true,
       blockAllMedia: true,
     }),
