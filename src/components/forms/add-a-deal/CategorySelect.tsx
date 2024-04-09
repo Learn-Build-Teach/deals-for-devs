81107fe43387fe4888c3e04679f3e2a3d6c08301
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-04-09 13:34:12 -0400

    fix: category select values not holding on form page reload

--- src/components/forms/add-a-deal/CategorySelect.tsx
+++ src/components/forms/add-a-deal/CategorySelect.tsx
@@ -17,9 +17,9 @@ export default function CategorySelect({
   return (
     <div className="flex flex-col gap-2">
       <span className="text-2xl font-extralight">Category *</span>
-      <Select onValueChange={onCategoryChange} value={value} required>
+      <Select onValueChange={onCategoryChange} value={value || undefined}>
         <SelectTrigger className="h-16 w-[334px] bg-transparent text-xl focus:ring-offset-teal-500">
-          <SelectValue placeholder="Please select a category" />
+          <SelectValue placeholder="Please select a Category" />
         </SelectTrigger>
         <SelectContent>
           {Object.values(Category)
