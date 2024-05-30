df20079bbcb8367ec319d82024ce6c037bb07aa4
James Q Quick <james.q.quick@gmail.com>
2024-05-30 14:05:01 -0500

    Feat: dynamic category routes (#131)
    dynamic category routes

--- src/components/deals/DealCard.tsx
+++ src/components/deals/DealCard.tsx
@@ -55,7 +55,7 @@ export default function DealCard({
       )}
       <h2 className="text-lg font-semibold tracking-tight">{deal.name}</h2>
       {deal.coupon && deal.couponPercent && (
-        <p className="-gap-y-1 bg-pale-gold absolute right-3 top-3 flex h-14 w-14 -rotate-12 flex-col items-center justify-center rounded-full  text-black shadow-md">
+        <p className="-gap-y-1 absolute right-3 top-3 flex h-14 w-14 -rotate-12 flex-col items-center justify-center rounded-full bg-pale-gold  text-black shadow-md">
           <span className="text-md -mb-1 font-bold">{deal.couponPercent}%</span>
           <span className="text-xs uppercase">off</span>
         </p>
