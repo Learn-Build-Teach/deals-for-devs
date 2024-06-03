bb8ea1639d44ea0abcb1f7eb7b31132bb504b4da
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-06-03 10:24:40 -0400

    chore: removed unused imports

--- src/app/(subscriber)/confirm/page.tsx
+++ src/app/(subscriber)/confirm/page.tsx
@@ -1,9 +1,9 @@
 import ResendConfirmationButton from '@/components/subscriber/ResendConfirmationButton'
 import { createValidateEmailLink } from '@/lib/utils'
 import { redirect } from 'next/navigation'
-import { createPreferencesLink } from '@/lib/utils'
 import { getOneSubscriberByToken } from '@/lib/queries'
 import VerifiedStatus from '@/components/subscriber/VerifiedStatus'
+import Link from 'next/link'
 
 interface ConfirmEmailProps {
   searchParams: {
@@ -29,11 +29,6 @@ export default async function ConfirmEmail({
 
   const { email, verified } = subscriber
 
-  if (verified) {
-    const preferencesLink = createPreferencesLink(tokenFromParams)
-    return redirect(preferencesLink)
-  }
-
   const validateEmailLink = createValidateEmailLink(tokenFromParams)
 
   return (
@@ -41,21 +36,42 @@ export default async function ConfirmEmail({
       {/* email and verified status */}
       <VerifiedStatus email={email} verified={verified as boolean} />
 
-      {/* call to action */}
-      <h1 className="my-4 text-2xl font-semibold tracking-tight md:mb-16 md:mt-10 md:text-7xl">
-        Please Verify Your Email!
-      </h1>
-      <p className=" mb-10 text-center text-xs leading-6 md:mb-20 md:text-2xl md:leading-10">
-        {` You'll receive an email shortly at `}
-        <span className="font-bold text-teal-500 underline">{email}</span>.
-        <br />
-        Click the link in your email to verify your account.
-      </p>
+      {/* not verified */}
+      {!verified && (
+        <>
+          {/* call to action */}
+          <h1 className="my-4 text-2xl font-semibold tracking-tight md:mb-16 md:mt-10 md:text-7xl">
+            Please Verify Your Email!
+          </h1>
+          <p className=" mb-10 text-center text-xs leading-6 md:mb-20 md:text-2xl md:leading-10">
+            {` You'll receive an email shortly at `}
+            <span className="font-bold text-teal-500 underline">{email}</span>.
+            <br />
+            Click the link in your email to verify your account.
+          </p>
 
-      <ResendConfirmationButton
-        email={email}
-        validateEmailLink={validateEmailLink}
-      />
+          <ResendConfirmationButton
+            email={email}
+            validateEmailLink={validateEmailLink}
+          />
+        </>
+      )}
+
+      {/* verified */}
+      {verified && (
+        <>
+          <h1 className="my-4 text-2xl font-semibold tracking-tight md:mb-16 md:mt-10 md:text-6xl">
+            Your email is verified, Thanks!
+          </h1>
+
+          <Link
+            href="/deals"
+            className="mt-2 rounded-lg bg-teal-600 p-4 py-4 text-xl hover:bg-teal-600/40 lg:text-4xl"
+          >
+            View All Deals
+          </Link>
+        </>
+      )}
     </main>
   )
 }
