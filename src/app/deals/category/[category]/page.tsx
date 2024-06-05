9a316ff22ffa325c086e1010f43bbf494e2f7209
jamesqquick <james.q.quick@gmail.com>
2024-06-05 14:20:46 -0500

    feat: display featured deals on home page

---
+++ src/app/deals/category/[category]/page.tsx
@@ -0,0 +1,44 @@
+import CategoryOptions from '@/components/CategoryOptions'
+import { redirect } from 'next/navigation'
+import { Category } from '@/types/Types'
+import { getApprovedDealsByCategory } from '@/lib/queries'
+import PageHeader from '@/components/PageHeader'
+import DealsList from '@/components/deals/DealsList'
+
+export default async function CategoryPage({
+  params,
+}: {
+  params: { category: string }
+}) {
+  if (!params.category) {
+    redirect('/')
+  }
+
+  const categoryString = decodeURIComponent(params.category).toUpperCase()
+
+  if (Object.keys(Category).indexOf(categoryString as Category) === -1) {
+    redirect('/')
+  }
+  const category = categoryString as Category
+  const deals = await getApprovedDealsByCategory(category)
+
+  return (
+    <div>
+      <div className="pb-10">
+        <PageHeader
+          title={category.toLocaleLowerCase()}
+          subtitle={`The best ${category.toLowerCase()} deals`}
+        />
+      </div>
+      <div className="pb-10">
+        <CategoryOptions />
+      </div>
+      {deals.length === 0 && (
+        <div className="text-center text-lg text-gray-500">
+          No deals found for this category
+        </div>
+      )}
+      <DealsList deals={deals} />
+    </div>
+  )
+}
---
+++ src/app/deals/page.tsx
@@ -0,0 +1,28 @@
+import CategoryOptions from '@/components/CategoryOptions'
+import { getApprovedDeals } from '@/lib/queries'
+import NeverMissADeal from '@/components/NeverMissADeal'
+import Separator from '@/components/Separator'
+import PageHeader from '@/components/PageHeader'
+import DealsList from '@/components/deals/DealsList'
+
+export default async function DealsPage() {
+  const deals = await getApprovedDeals(20)
+  //TODO: handle error
+  return (
+    <div>
+      <div>
+        <PageHeader
+          title="View all deals"
+          subtitle="From online courses to conference tickets, we've curated the best ongoing deals!"
+        />
+      </div>
+      <Separator className="my-16" />
+
+      <CategoryOptions />
+      <div className="py-10">
+        <DealsList deals={deals} />
+      </div>
+      <NeverMissADeal />
+    </div>
+  )
+}
