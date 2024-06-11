b0ad432cf55cdc2b6b4082c463b10733870fdf12
Ryan Furrer <ryandotfurrer@gmail.com>
2024-06-11 11:54:41 -0400

    fix(admin-directory): moved clerk provider, changed admin directory
    moved clerk provider to admin/dashboard layout only, changed (admin) to admin and moved auth pages

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 //info: must import env types file so validation is run
-import { auth } from '@clerk/nextjs'
+import { ClerkProvider, auth } from '@clerk/nextjs'
 import { isAdminUser } from '@/utils/auth'
 import { redirect } from 'next/navigation'
 import AdminNav from '@/components/dashboard/AdminNav'
@@ -26,13 +26,15 @@ export default async function Home({
   }
 
   return (
-    <main className="mx-auto space-y-12 px-4 pb-10">
-      <section className="mx-auto grid gap-8 space-y-12 px-4  pb-10 lg:grid-cols-3 lg:space-y-0">
-        <div className="mx-auto lg:col-span-1">
-          <AdminNav />
-        </div>
-        <div className="mx-auto lg:col-span-2">{children}</div>
-      </section>
-    </main>
+    <ClerkProvider>
+      <main className="mx-auto space-y-12 px-4 pb-10">
+        <section className="mx-auto grid gap-8 space-y-12 px-4  pb-10 lg:grid-cols-3 lg:space-y-0">
+          <div className="mx-auto lg:col-span-1">
+            <AdminNav />
+          </div>
+          <div className="mx-auto lg:col-span-2">{children}</div>
+        </section>
+      </main>
+    </ClerkProvider>
   )
 }
--- src/components/dashboard/AdminNav.tsx
+++ src/components/dashboard/AdminNav.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { useState } from 'react'
 import Link from 'next/link'
 
 export default function AdminNav() {
