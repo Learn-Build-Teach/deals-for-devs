c4ff0168096274dbb4cebaedd4817d8cd82563b0
Christine <shecoder30@gmail.com>
2024-06-25 03:51:31 +0000

    fix: move to built with logos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -41,16 +41,6 @@ export default function Footer() {
         <span className="mb-2 text-sm font-medium leading-8 text-white md:text-3xl">
           Built With
         </span>
-<span className="mb-2 text-sm font-medium leading-8 text-white md:text-3xl">
-          Found a bug or want a new feature?{' '}
-          <a
-            href="https://github.com/Learn-Build-Teach/deals-for-devs/issues/new/choose"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Raise your issue on GitHub
-          </a>
-  </span>
         {/* built with logos */}
         <div className="flex flex-wrap items-center justify-center gap-4 md:gap-10">
           {techStack.map((sponsor, index) => (
@@ -74,7 +64,16 @@ export default function Footer() {
             />
           </a>
         </div>
-
+        <span className="mb-2 text-sm font-medium leading-8 text-white md:text-3xl">
+          Found a bug or want a new feature?{' '}
+          <a
+            href="https://github.com/Learn-Build-Teach/deals-for-devs/issues/new/choose"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Raise your issue on GitHub
+          </a>
+  </span>
         {/* james quick social link */}
         {/* <Link
           className="mt-8 text-sm font-medium text-teal-500 md:text-xl"
