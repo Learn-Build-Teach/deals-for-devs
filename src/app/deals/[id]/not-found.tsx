e1b948094bf7a6c64d14893bdd793983d37a6015
Emma Dawson <emma.l.dawson@gmail.com>
2024-06-27 10:16:28 +0200

    fix: add main landmark to pages

--- src/app/deals/[id]/not-found.tsx
+++ src/app/deals/[id]/not-found.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 export default function Deal404() {
   return (
-    <div>
+    <main>
       <div className="pb-10">
         <PageHeader title="Deal not found" />
         <Link className="text-lg text-white underline" href="/deals">
           Back to all deals
         </Link>
       </div>
-    </div>
+    </main>
   )
 }
