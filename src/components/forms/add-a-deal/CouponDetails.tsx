0f7ab355b1ba58ead91a70dfe42eb20ad4e628db
klae32 <techie.r13@gmail.com>
2024-07-04 07:43:19 -0600

    fix: updated the button color to be brighter (#211)
    Co-authored-by: James Q Quick <james.q.quick@gmail.com>

--- src/components/forms/add-a-deal/CouponDetails.tsx
+++ src/components/forms/add-a-deal/CouponDetails.tsx
@@ -90,7 +90,7 @@ export default function CouponDetails() {
 
             <button
               type="submit"
-              className="mt-2 rounded-lg bg-teal-600 py-4 text-lg text-black disabled:bg-teal-600/30 lg:-mt-4 lg:py-7 lg:text-2xl"
+              className="mt-2 rounded-lg bg-teal-500 py-4 text-lg text-black disabled:bg-teal-600/30 lg:-mt-4 lg:py-7 lg:text-2xl"
               aria-label="Click to continue"
             >
               Continue
--- src/components/forms/add-a-deal/ReviewDeal.tsx
+++ src/components/forms/add-a-deal/ReviewDeal.tsx
@@ -78,7 +78,7 @@ export default function ReviewDeal() {
           />
           <button
             type="button"
-            className="mt-5 rounded-lg bg-teal-600 py-4 text-lg text-black disabled:bg-teal-600/30 lg:mt-10 lg:py-7 lg:text-2xl"
+            className="mt-5 rounded-lg bg-teal-500 py-4 text-lg text-black disabled:bg-teal-600/30 lg:mt-10 lg:py-7 lg:text-2xl"
             aria-label="Click to continue"
             onClick={validateAndSubmit}
           >
