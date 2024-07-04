eff163a6377fc7fd6a828d088973d82f99547536
jamesqquick <james.q.quick@gmail.com>
2024-07-04 08:20:06 -0500

    chore: added text for disclaimer page

--- src/app/disclaimer/page.tsx
+++ src/app/disclaimer/page.tsx
@@ -1,67 +1,20 @@
+import PageHeader from '@/components/PageHeader'
+
 export const revalidate = 120
 
 export default async function Disclaimer() {
   return (
     <div>
-      <h1 className="pb-5 text-center text-2xl text-gray-300">Disclaimer</h1>
-      <p className=" text-lrg text-gray-300">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-        repudiandae tempore unde rem autem iure rerum eos nostrum numquam,
-        temporibus assumenda amet harum magni a vitae nisi nesciunt eaque, ab
-        iste cum? Cum dolorem esse officia minus nemo labore est quos
-        consectetur facere? Facilis doloribus atque, tempora eveniet doloremque
-        inventore maxime error voluptatum architecto, culpa aliquid omnis.
-        Consequuntur, consequatur! Consectetur tempore quidem, iste fuga quos
-        magnam sit iusto nemo, ab ratione in rerum eius fugiat totam harum
-        excepturi. Dolores architecto dolore, nobis, veniam odit amet nostrum
-        magnam dicta corporis odio inventore in minima aperiam? Magnam ipsum
-        officia non. Repellat deleniti temporibus laudantium accusamus soluta,
-        tenetur ducimus inventore dolores quas voluptas consequuntur aliquid,
-        debitis maiores nobis unde labore ex fuga voluptates consequatur ipsam
-        iure vel porro iusto. Sint fuga sunt tempore ab et, vel voluptate
-        perferendis corrupti quidem animi, non illum, minus eos laborum
-        exercitationem voluptatibus deleniti rem. Voluptate at autem harum fugit
-        itaque excepturi aliquam, sunt libero earum architecto, asperiores
-        voluptatum saepe possimus qui. Sequi consequatur quia eos nulla labore a
-        veritatis obcaecati laudantium est quas quod consequuntur et eveniet,
-        veniam deleniti alias minima. Eum dolorum accusantium obcaecati illo
-        molestiae, maiores nemo dolores placeat expedita velit nisi repudiandae
-        ipsum odit. Mollitia accusamus temporibus, eos repellat, illum eaque
-        veniam fuga nihil dolorum, inventore voluptate veritatis aut magnam
-        omnis laboriosam laudantium quos. In quia et totam consequuntur fugiat,
-        tempore qui numquam earum impedit non magnam recusandae accusantium
-        magni veniam fuga repellendus necessitatibus id exercitationem minima
-        amet inventore sequi excepturi! Veniam expedita, minima obcaecati
-        assumenda laboriosam recusandae inventore, voluptates sit saepe itaque
-        fuga consequuntur suscipit officiis. Ipsam non aperiam reprehenderit
-        laudantium optio consequatur quas perspiciatis, vel qui, incidunt cum
-        possimus. Unde placeat doloribus molestias qui alias error neque
-        aliquid, dignissimos dolorum incidunt quia nesciunt totam doloremque
-        nisi ex quaerat eius assumenda tempore blanditiis reprehenderit iste
-        quasi molestiae laudantium. Voluptatum neque cumque eveniet assumenda
-        facere sint sunt excepturi harum recusandae obcaecati reiciendis, nulla,
-        laudantium eius molestias eos consequuntur nesciunt, necessitatibus
-        blanditiis! Magni iste praesentium iure ipsum deserunt esse, nostrum
-        rerum! Autem magni nostrum qui repellendus, sequi dignissimos id nihil
-        consectetur accusantium ut dicta pariatur harum, ea laudantium quisquam?
-        Dolorem delectus odio asperiores, minus voluptates quaerat, dicta
-        aliquam nemo cum ipsam ipsa. Reprehenderit minus corporis voluptates
-        tempore nisi impedit fugit quod veniam quisquam sapiente quasi optio
-        quis libero similique ea id vero repellat labore blanditiis earum
-        repellendus, sit qui facere! Asperiores mollitia autem perspiciatis modi
-        odit dolorum quam, distinctio esse culpa cupiditate numquam perferendis
-        facilis nihil veniam quas dolor alias eum. Et in aperiam doloremque
-        reprehenderit, unde provident corporis! Esse ad expedita rem officiis
-        asperiores! Minima alias incidunt enim nesciunt earum voluptate id
-        dolorem temporibus veritatis ut. Totam provident saepe eligendi ullam
-        inventore dolore repellat quae aperiam amet illo consequatur quas
-        officiis sit consectetur eveniet quis voluptates non numquam eius, nobis
-        tenetur dolorum optio? Similique odit dolorum aliquid quaerat molestiae
-        molestias quas a voluptate doloribus, laborum, dolor deserunt aperiam
-        unde id deleniti commodi, facilis doloremque eveniet assumenda
-        architecto esse perspiciatis voluptatibus sed. Natus, dolor
-        perspiciatis.
-      </p>
+      <div className="pb-10">
+        <PageHeader title="Disclaimer" />
+      </div>
+      <div className="pb-10">
+        <p className=" text-lrg text-gray-300">
+          Some deals may include affiliate links. Proceeds from these affiliate
+          links are invested back into the developer community through giveaways
+          on courses and other resources.
+        </p>
+      </div>
     </div>
   )
 }
