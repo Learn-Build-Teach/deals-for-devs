9007bd06b0c431086c446e2d552e49bde09fe886
Emma Dawson <emma.l.dawson@gmail.com>
2024-07-12 14:19:46 +0200

    fix: update overlay to use native html dialog component

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ReactNode } from 'react'
+import { ReactNode, useEffect, useRef } from 'react'
 import { FaTimes } from 'react-icons/fa'
 
 const Overlay = ({
@@ -12,22 +12,28 @@ const Overlay = ({
   onClose: () => void
   children: ReactNode
 }) => {
+  const dialogRef = useRef<HTMLDialogElement>(null)
+
+  useEffect(() => {
+    if (isOpen) {
+      dialogRef.current?.showModal()
+    } else {
+      dialogRef.current?.close()
+    }
+  })
   return (
-    <div
-      className={`fixed inset-0  flex items-center justify-center p-10 ${
-        isOpen ? 'block' : 'hidden'
-      } z-50 bg-gray-950/[.80] `}
+    <dialog
+      ref={dialogRef}
+      className="hide-scrollbar relative h-[90vh]  w-full max-w-4xl rounded-xl  bg-gray-900 px-4 pt-16 shadow-lg backdrop:bg-gray-950/[.80] md:px-8 md:pt-16"
     >
-      <div className="hide-scrollbar relative h-[90vh]  w-full max-w-4xl overflow-y-scroll rounded-xl  bg-gray-900 px-4 pt-16 shadow-lg md:px-8 md:pt-16">
-        <button
-          onClick={onClose}
-          className="fixed right-3 top-3 text-gray-400 transition-colors hover:text-white focus:outline-none"
-        >
-          <FaTimes className="h-10" />
-        </button>
-        {children}
-      </div>
-    </div>
+      <button
+        onClick={onClose}
+        className="fixed right-3 top-3 text-gray-400 transition-colors hover:text-white"
+      >
+        <FaTimes className="h-10" />
+      </button>
+      {children}
+    </dialog>
   )
 }
 
