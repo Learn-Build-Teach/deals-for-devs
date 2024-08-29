aab3c9a691a755c0a4d698e096fb9a06ccf07e1a
jamesqquick <james.q.quick@gmail.com>
2024-08-29 14:50:33 -0500

    fix: incorrect button variant in calendar component

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -38,7 +38,7 @@ function Calendar({
         row: 'flex w-full mt-2',
         cell: 'h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-slate-100/50 [&:has([aria-selected])]:bg-slate-100 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20 dark:[&:has([aria-selected].day-outside)]:bg-slate-800/50 dark:[&:has([aria-selected])]:bg-slate-800',
         day: cn(
-          buttonVariants({ variant: 'ghost' }),
+          buttonVariants({ variant: 'ghost-light' }),
           'h-9 w-9 p-0 font-normal aria-selected:opacity-100'
         ),
         day_range_end: 'day-range-end',
