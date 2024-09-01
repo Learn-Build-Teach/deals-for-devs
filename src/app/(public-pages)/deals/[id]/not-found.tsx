0c7deee96852973447c677f8abb354865f7babda
James Q Quick <james.q.quick@gmail.com>
2024-09-01 11:01:19 -0500

    fix: add container to 404 (#268)

--- src/app/(public-pages)/deals/[id]/not-found.tsx
+++ src/app/(public-pages)/deals/[id]/not-found.tsx
@@ -1,16 +1,17 @@
+import Container from '@/components/Container'
 import PageHeader from '@/components/PageHeader'
 import Link from 'next/link'
 import React from 'react'
 
 export default function Deal404() {
   return (
-    <main>
-      <div className="pb-10">
+    <Container className="pb-10">
+      <main>
         <PageHeader heading="Deal not found" />
         <Link className="text-lg text-white underline" href="/deals">
           Back to all deals
         </Link>
-      </div>
-    </main>
+      </main>
+    </Container>
   )
 }
