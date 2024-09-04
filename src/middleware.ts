49d833f59f20e5c9c7e7ea25966b143c79092212
jamesqquick <james.q.quick@gmail.com>
2024-09-04 13:30:33 -0500

    chore: updated clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,12 @@
-import { authMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default authMiddleware({
-  publicRoutes: (req) => !req.url.includes('/dashboard'),
-})
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 }
