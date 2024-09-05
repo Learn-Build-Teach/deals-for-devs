5a7c8004439d6331f5ed4d86ed95477af80ad9d2
jamesqquick <james.q.quick@gmail.com>
2024-09-05 10:58:59 -0500

    feat: added swr for requests and caching and usedebounce hook

--- src/components/search/SearchContext.jsx
+++ src/components/search/SearchContext.jsx
@@ -1,18 +1,54 @@
-'use client';
-import React, { createContext, useContext, useState } from 'react';
+'use client'
+import React, { createContext, useContext, useState } from 'react'
+import useSWR from 'swr'
+import { useDebounce } from '@uidotdev/usehooks'
 
-const SearchContext = createContext();
+const fetcher = async (url) => {
+  const res = await fetch(url)
+  if (!res.ok) {
+    const error = new Error('An error occurred while fetching the data.')
+    error.info = await res.json()
+    error.status = res.status
+    throw error
+  }
 
-export function useSearch() {
-  return useContext(SearchContext);
+  return res.json()
+}
+
+const SearchContext = createContext()
+
+export function useSearch(id) {
+  return useContext(SearchContext)
 }
 
 export function SearchProvider({ children }) {
-  const [searchOpen, setSearchOpen] = useState(false);
+  const [searchOpen, setSearchOpen] = useState(false)
+  const [query, setQuery] = useState('')
+  const debouncedSearchTerm = useDebounce(query, 300)
+
+  const { data, error, isLoading } = useSWR(
+    debouncedSearchTerm ?
+      `/api/deals/search?query=${debouncedSearchTerm}`
+    : null,
+    fetcher,
+    {
+      revalidateIfStale: false,
+    }
+  )
 
   return (
-    <SearchContext.Provider value={{ searchOpen, setSearchOpen }}>
+    <SearchContext.Provider
+      value={{
+        searchOpen,
+        setSearchOpen,
+        data,
+        error,
+        isLoading,
+        setQuery,
+        query,
+      }}
+    >
       {children}
     </SearchContext.Provider>
-  );
+  )
 }
