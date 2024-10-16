7fb97deacd730eb2d5740e96dd0b7a61cdc5de89
Scott D'Alessandro <sdaless22@gmail.com>
2024-10-16 10:05:22 -0400

    feat: email admins new deals (#284)
    * feat: add email template to notify admins when a new deal is submitted
    
    * refactor: make app domain a prop, add npm command for react-email dev server
    
    * feat: add api/admin routing
    
    * refactor: update email temp props, resend sendAdminEmail method
    
    * feat: add inngest and sendAdminEmail alerts
    
    * feat: add action to trigger admin new deal email
    
    * chore: add inngest env vars to examples
    
    * feat(inngest): add inngest script
    
    * chore: add inngest-cli
    
    * chore: reinstall inngest-clie
    
    * chore: update env vars
    
    * chore: refactor sendEmails to action, remove admin api route
    
    * chore: refactor email links and lint route directory
    
    * chore: fix urls in email template
    
    * chore: refactor try/catch to a single block

---
+++ src/emails/emailAdminNewDeal.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react'
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Text,
+  Tailwind,
+  Link,
+} from '@react-email/components'
+
+interface EmailTemplateProps {
+  dealId: string
+  appDomain: string
+}
+
+export const emailAdminNewDeal = (props: EmailTemplateProps) => (
+  <Tailwind>
+    <Html lang="en">
+      <Head />
+      <Preview>New Deal Submitted</Preview>
+      <Body className="mx-auto bg-white p-4">
+        <Container>
+          <Section>
+            <Img
+              src={`${props.appDomain}/logo-teal.png`}
+              width="60"
+              alt="Deals for Devs"
+            />
+          </Section>
+          <Heading style={h1}>Admin - Review new deal</Heading>
+          <Section className="py-6">
+            <Link
+              href={`${props.appDomain}/dashboard/deals/${props.dealId}`}
+              className="rounded-lg border bg-teal-500 px-5 py-3 text-xl text-white no-underline"
+            >
+              Review Deal
+            </Link>
+          </Section>
+          <Text className="text-lg">
+            If you are having trouble clicking the button, copy and paste the
+            URL below 👇
+          </Text>
+          <Section style={codeBox}>
+            <Text className="text-lg">
+              {`${props.appDomain}/dashboard/deals/${props.dealId}`}
+            </Text>
+          </Section>
+          <Text className="text-sm">
+            Ps. You received this email because you are an admin user for Deals
+            For Devs.
+          </Text>
+        </Container>
+      </Body>
+    </Html>
+  </Tailwind>
+)
+
+emailAdminNewDeal.PreviewProps = {
+  dealId: 'the-deal-id-here',
+  appDomain: 'http://localhost:3000',
+} as EmailTemplateProps
+
+export default emailAdminNewDeal
+
+const h1 = {
+  color: '#1d1c1d',
+  fontSize: '24px',
+  fontWeight: '700',
+  margin: '10px 0',
+  padding: '0',
+  lineHeight: '42px',
+}
+
+const codeBox = {
+  background: 'rgb(245, 244, 245)',
+  borderRadius: '10px',
+  marginBottom: '15px',
+  padding: '10px',
+}
